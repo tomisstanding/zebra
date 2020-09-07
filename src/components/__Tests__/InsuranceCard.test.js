96e926e18650c5aec278a8456323c46ec2769d42
Thomas Standing <thomas.standing@macys.com>
2020-09-07 10:08:56 -0500

    move tests into folder

--- src/components/__Tests__/InsuranceCard.test.js
+++ src/components/__Tests__/InsuranceCard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import InsuranceCard from './InsuranceCard';
+import InsuranceCard from '../InsuranceCard/InsuranceCard';
 import { insuranceData } from '../../assets/data';
 
 describe('InsuranceCard',  () => {
